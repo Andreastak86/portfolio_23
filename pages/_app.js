c0208eb62bcd4aa80d103cba62746b95e65d794b
Andreastak <104064121+Andreastak86@users.noreply.github.com>
2023-04-17 21:41:55 +0200

    add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { hotjar } from "react-hotjar";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+        hotjar.initialize(3454834, 6);
+    }, []);
+    return <Component {...pageProps} />;
 }
